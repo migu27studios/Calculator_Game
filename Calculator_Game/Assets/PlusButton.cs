19827a51399d244e93846f7c9e09d321e9538830
Unknown <migu27studios@gmail.com>
2017-04-03 10:57:44 -0600

    Added Button and Began to code

---
+++ Calculator_Game/Assets/PlusButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlusButton : MonoBehaviour {
+
+    public ValueOneInput valueOneInput;
+    float additionTest = 2f;
+
+    float Addition (float valueOne)
+    {
+        float additionFinal = valueOne + additionTest;
+        return additionFinal;
+    }
+
+    public void ButtonClick ()
+    {
+        print(Addition(valueOneInput.InputOneToFloat()));
+    }
+}
---
+++ Calculator_Game/Assets/ValueOneInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ValueOneInput : MonoBehaviour {
+
+    public InputField valueOne;
+
+    public float InputOneToFloat ()
+    {
+        return float.Parse(valueOne.text);
+    }
+}
